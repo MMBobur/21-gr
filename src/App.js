67773c8d7a794cebbfe343f377ac9f61fcee7029
asilbekcod <isarvakoriya@gmail.com>
2022-12-23 11:56:04 +0500

    home

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './asilbek/Navbar/Navbar';
 import Home from './asilbek/Home/Home';
-import Animals from './asilbek/Animals/Animals';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
-      <Animals />
     </div>
   );
 }
